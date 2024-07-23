ec375ed305fa17df057bed25c90cb0815c1e1c16
Arshia Naseri <arshia.naseri.w@outlook.com>
2024-07-23 03:52:53 -0400

    + Web title and logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
-
 function App() {
-  return <></>;
+  return (
+    <>
+      <section className=" bg-red-300 w-screen h-screen flex justify-center items-center">
+        <div className=" font-serif text-8xl">Hello</div>
+      </section>
+    </>
+  );
 }
 
 export default App;
