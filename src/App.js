75cda08ea3a53f3269f81b089fb3cd2f46c50de0
Arshia Naseri <arshia.naseri.w@outlook.com>
2024-07-26 03:41:02 -0400

    + Desktop view added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React, { useEffect, useState } from "react";
 import Desktop from "./Views/desktop";
 import profilePic from "./Assets/profile-pic.jpg";
 import contactFile from "./Assets/Alireza-Pakmehr.vcf";
 
 function App() {
+  const [showTooltip, setShowTooltip] = useState("");
+  const LinkedInURL = "http://linkedin.com/in/alireza-pak-mehr-411982288";
+  const InstagramURL = "https://www.instagram.com/chessmastersacademy/";
+  const email = "rhchessmastersacademy@gmail.com";
   const shareURL = async (e) => {
     try {
       await navigator.share({
@@ -17,12 +22,37 @@ function App() {
     }
   };
 
+  const copyText = async (e, text, dscr = "") => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setShowTooltip(dscr);
+      // console.log("DONE");
+      // alert("Copied!");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowTooltip("");
+    }, 1000);
+
+    // Clean up timer
+    return () => clearTimeout(timer);
+  }, [showTooltip]);
+
   return (
     <>
       <Desktop
         profilePic={profilePic}
         contactFile={contactFile}
         shareURL={shareURL}
+        email={email}
+        LinkedInURL={LinkedInURL}
+        InstagramURL={InstagramURL}
+        copyText={copyText}
+        showTooltip={showTooltip}
       />
     </>
   );
--- src/Views/desktop.js
+++ src/Views/desktop.js
@@ -1,12 +1,26 @@
-import { IoShareOutline, IoCloseCircle } from "react-icons/io5";
+import {
+  IoShareOutline,
+  IoCloseCircle,
+  IoCopy,
+  IoCheckmarkSharp,
+} from "react-icons/io5";
 
-function Desktop({ profilePic, contactFile, shareURL }) {
+function Desktop({
+  profilePic,
+  contactFile,
+  shareURL,
+  email,
+  LinkedInURL,
+  InstagramURL,
+  copyText,
+  showTooltip,
+}) {
   return (
     <>
       {/* Desktop */}
       <section className="flex h-screen w-screen items-center justify-center bg-slate-50 font-sans max-sm:hidden">
         <main className="relative h-3/4 w-3/4 max-w-xl rounded-lg border border-black border-opacity-20 bg-[rgba(105,100,100,.7)] shadow-2xl shadow-[rgba(0,0,0,.7)]">
-          <section className="border-border flex size-full flex-col gap-16 rounded-lg border p-3">
+          <section className="border-border flex size-full flex-col gap-9 rounded-lg border p-3">
             {/* Nav Bar */}
             <nav>
               <ul className="flex items-center justify-end">
@@ -50,7 +64,84 @@ function Desktop({ profilePic, contactFile, shareURL }) {
               </section>
             </section>
             {/* Detailed info */}
-            <section className="grid grid-cols-2 bg-slate-400"></section>
+            <section className="grid grid-cols-[30%_70%] overflow-x-hidden overflow-y-scroll">
+              <div className="cell-attr" dir="rtl">
+                mobile
+              </div>
+              <div className="cell-attr">(437) 220-2743</div>
+              <div className="cell-attr" dir="rtl">
+                work email
+              </div>
+              <div className="cell-attr flex items-center gap-2">
+                <div className="hide-overflow-text">{email}</div>
+                {showTooltip !== "email" ? (
+                  <IoCopy
+                    title="Copy"
+                    className="hover:cursor-pointer hover:text-neutral-300"
+                    onClick={(e) => copyText(e, email, "email")}
+                  />
+                ) : (
+                  <IoCheckmarkSharp size="18px" />
+                )}
+              </div>
+              <div className="cell-attr" dir="rtl">
+                LinkedIn
+              </div>
+              <div className="cell-attr flex items-center gap-2">
+                <a
+                  href={LinkedInURL}
+                  className="hide-overflow-text hover:underline"
+                >
+                  {LinkedInURL}
+                </a>
+                {showTooltip !== "linkedin" ? (
+                  <IoCopy
+                    title="Copy"
+                    className="hover:cursor-pointer hover:text-neutral-300"
+                    onClick={(e) => copyText(e, LinkedInURL, "linkedin")}
+                  />
+                ) : (
+                  <IoCheckmarkSharp size="18px" />
+                )}
+              </div>
+              <div className="cell-attr" dir="rtl">
+                Instagram
+              </div>
+              <div className="cell-attr flex items-center gap-2">
+                <a
+                  href={InstagramURL}
+                  className="hide-overflow-text hover:underline"
+                >
+                  {InstagramURL}
+                </a>
+                {showTooltip !== "instagram" ? (
+                  <IoCopy
+                    title="Copy"
+                    className="hover:cursor-pointer hover:text-neutral-300"
+                    onClick={(e) => copyText(e, InstagramURL, "instagram")}
+                  />
+                ) : (
+                  <IoCheckmarkSharp size="18px" />
+                )}
+              </div>
+              <div className="cell-attr last-row" dir="rtl">
+                note
+              </div>
+              <div className="cell-attr last-row">
+                <p>
+                  Are you looking to improve your chess skills? I can help! I'm
+                  a chess instructor with a FIDE rating, which means I've been
+                  recognized internationally for my chess expertise. In North
+                  America, especially in the United States, most players have
+                  CFC (Chess Federation of Canada) ratings. But FIDE ratings are
+                  known worldwide for their reliability and prestige. When you
+                  choose me as your chess instructor, you're getting top-notch
+                  guidance based on my FIDE rating, ensuring you receive
+                  high-quality lessons. Join me on a chess journey that's
+                  respected all around the world.
+                </p>
+              </div>
+            </section>
           </section>
         </main>
       </section>
